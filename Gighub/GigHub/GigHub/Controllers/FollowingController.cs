8253974b0b338241a993743527d64a58ee122870
Eric Downey <edowney271@gmail.com>
2017-01-03 21:29:54 -0500

    Final Commit

--- Gighub/GigHub/GigHub/Controllers/FollowingController.cs
+++ Gighub/GigHub/GigHub/Controllers/FollowingController.cs
@@ -1,32 +1,35 @@
-﻿using GigHub.Models;
-using Microsoft.AspNet.Identity;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
-    [Authorize]
-    public class AttendancesController : ApiController
+    public class FollowingController : ApiController
     {
         private ApplicationDbContext _context;
 
-        public AttendancesController()
+        public FollowingController()
         {
             _context = new ApplicationDbContext();
         }
 
         [HttpPost]
-        public IHttpActionResult Attend(AttendanceDto dto)
+        public IHttpActionResult Attend(FollowingDto dto)
         {
             var userId = User.Identity.GetUserId();
 
-            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
-                return BadRequest("The attendance already exists!");
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.FollowId))
+                return BadRequest("You've already followed this artist!");
 
             var attendance = new Attendance
             {
-                GigId = dto.GigId,
+                GigId = dto.FollowId,
                 AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
